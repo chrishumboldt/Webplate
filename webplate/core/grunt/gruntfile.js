3edb23353353b3d39cc00eca7ad486391e4ffebd
Chris Humboldt <chrismodem@gmail.com>
2014-05-03 15:47:36 +0200

    Compressed CSS

--- webplate/core/grunt/gruntfile.js
+++ webplate/core/grunt/gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function(grunt) {
 		sass: {
 			dist: {
 				options: {
-					style: 'expanded'
+					style: 'compressed'
 				},
 				files: {
 					'../css/webplate.css': '../sass/web-engine.scss'
