66bd128c312f78cc5eb1be553bd300d38f622cbd
Chris Humboldt <chrismodem@gmail.com>
2016-01-05 09:16:19 +0200

    Font Awesome fix.

--- engine/js/core.js
+++ engine/js/core.js
@@ -200,7 +200,7 @@
 		project: {
 			css: $pathProject + 'css/',
 			iconFont: {
-				fontAwesome: $pathProject + 'font-awesome/css/font-awesome.min.css',
+				fontAwesome: $pathProject + 'icon-font/font-awesome/css/font-awesome.min.css',
 				icoMoon: $pathProject + 'icon-font/icomoon/style.css'
 			},
 			js: $pathProject + 'js/',
