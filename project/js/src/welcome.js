108a2400cc27b44b276cde551f9daf983a905704
Chris Humboldt <chrismodem@gmail.com>
2016-01-08 21:55:27 +0200

    Some changes to tools.

--- project/js/src/welcome.js
+++ project/js/src/welcome.js
@@ -4,7 +4,15 @@
  * Author: Chris Humboldt
  */
 
+<<<<<<< Updated upstream
 web.scrollTo(".scroll-to", -88);
 new buttonplate({
 	selector: '.btn-banner'
 });
+=======
+web.scrollTo({
+   offset: -68,
+   offsetLarge: -88
+});
+new buttonplate('.btn-banner');
+>>>>>>> Stashed changes
