3cdf5af6731b8e2bf3d0942bd35d834fa7419562
Chris Humboldt <chrismodem@gmail.com>
2016-01-25 10:28:25 +0200

    Fixes to overwrites and core file.

--- engine/gulpfile.js
+++ engine/gulpfile.js
@@ -178,7 +178,7 @@ gulp.task('js', function() {
 				$concat = true;
 				for (var $i2 = 0, $len2 = $config.build[$i].js.length; $i2 < $len2; $i2++) {
 					if ($i2 === 0) {
-						$arConcatJS.push('./js/overwrite.js');
+						$arConcatJS.push('./js/overwrite.min.js');
 					}
 					$arConcatJS.push('../project/js/' + $config.build[$i].js[$i2]);
 				}
