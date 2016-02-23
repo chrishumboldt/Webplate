62f200ff9393141c81ac21c02346406f9dfdd128
Chris Humboldt <chrismodem@gmail.com>
2016-02-23 16:01:54 +0200

    Update to tools.js.

--- engine/js/src/tools.js
+++ engine/js/src/tools.js
@@ -396,12 +396,12 @@ var web = function() {
 	// As per Aliceljm
 	// http://stackoverflow.com/questions/15900485/correct-way-to-convert-size-in-bytes-to-kb-mb-gb-in-javascript
 	var formatBytes = function($bytes, $decimals) {
-		if (bytes == 0) return '0 Byte';
-		var k = 1000;
-		var dm = decimals + 1 || 3;
-		var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-		var i = Math.floor(Math.log(bytes) / Math.log(k));
-		return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+		if ($bytes == 0) return '0 Byte';
+		var $k = 1000;
+		var $dm = $decimals + 1 || 3;
+		var $sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+		var $i = Math.floor(Math.log($bytes) / Math.log($k));
+		return parseFloat(($bytes / Math.pow($k, $i)).toFixed($dm)) + ' ' + $sizes[$i];
 	}
 	var getExtension = function($file) {
 		return $file.split('.').pop().toLowerCase();
