1bb23e109b6fdaae2381401287c5e1445b203df4
Chris Humboldt <chrismodem@gmail.com>
2016-03-18 12:14:39 +0200

    Directory fixes.

---
+++ project/build/js/app.js
@@ -0,0 +1,9 @@
+/**
+ * File: project/build/js/app.js
+ * Type: Javascript app file
+ * Author: Chris Humboldt
+ * NOTE: This log is used to generate the js min file so
+ * that the js directory is included in the GIT repo.
+ */
+
+web.log('The app file is loading...');
