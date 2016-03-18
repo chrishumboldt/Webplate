169c0427a7eeb52ec76aee95f58b3b9fff48dff1
Chris Humboldt <chrismodem@gmail.com>
2016-03-18 12:22:24 +0200

    Remove build requirement.

--- project/js/app.js
+++ project/js/app.js
@@ -2,8 +2,8 @@
  * File: project/build/js/app.js
  * Type: Javascript app file
  * Author: Chris Humboldt
- * NOTE: This log is used to generate the js min file so
- * that the js directory is included in the GIT repo.
+ * NOTE: This log is used to make sure that the
+ * js directory is included in the GIT repo.
  */
 
 web.log('The app file has loaded successfully...');
