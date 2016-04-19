44931ad6f244c56a5e6d5d0df54fbeed2d823f55
Chris Humboldt <chrismodem@gmail.com>
2016-04-19 11:00:33 +0200

    Updated Tabplate component.

--- engine/component/tabplate/js/tabplate.js
+++ engine/component/tabplate/js/tabplate.js
@@ -9,89 +9,63 @@
 // Tools
 
 var $tabplateDefault = {
-	selector: '#tabplate-triggers',
-	animate: false,
-	tabs: '#tabplate-tabs'
+	selector: '#tab-triggers',
+	tabs: '#tab-content',
+	animate: true
 }
 
 var tabplate = function($userOptions) {
-	var $selector = ($userOptions && $userOptions.selector) ? $userOptions.selector : $tabplateDefault.selector;
-	var $selectorType = $selector.charAt(0).toString();
-	if ($selectorType === '#' && $selector.indexOf('.') < 0) {
-		new tabplateComponent(document.getElementById($selector.substring(1)), $userOptions);
-	} else {
-		var $elements = document.querySelectorAll($selector);
-		for (var $i = 0; $i < $elements.length; $i++) {
-			new tabplateComponent($elements[$i], $userOptions);
-		}
-	}
-};
-
-var tabplateComponent = function($this, $userOptions) {
-	// Variables
-	var $self = $this;
-	var $tabContent;
-	var $tabs;
+	// Setup
+	var $self = this;
 
 	// Options
 	$userOptions = $userOptions || false;
 	$self.options = {
-		animate: (typeof $userOptions.animate !== 'undefined') ? $userOptions.animate : $tabplateDefault.animate,
+		selector: $userOptions.selector || $tabplateDefault.selector,
 		tabs: $userOptions.tabs || $tabplateDefault.tabs,
-	};
-
-	// Public functions
-	$self.changeTab = function($index) {
-		web.classRemove($self.querySelector('li.active'), 'active');
-		web.classAdd($self.querySelector('li:nth-child(' + ($index + 1) + ')'), 'active');
-		web.classRemove($tabs.querySelector('li.active'), 'active');
-		web.classAdd($tabs.querySelector('li:nth-child(' + ($index + 1) + ')'), 'active');
-		if ($self.options.animate.toString() == 'true') {
-			$tabs.style.height = $tabs.querySelector('li.active').clientHeight + 'px';
-		}
-	};
-
-	// Internal functions
-	function tabSetup() {
-		if ($self.options.tabs.charAt(0) === '#') {
-			$tabs = document.getElementById($self.options.tabs.substring(1));
-		} else {
-			$tabs = document.querySelector($self.options.tabs);
-		}
-
-		if (web.exists($tabs)) {
-			web.classAdd($tabs, 'tabplate-tabs');
-			web.classAdd($tabs.querySelector('li:first-child'), 'active');
-
-			if ($self.options.animate.toString() == 'true') {
-				$tabs.style.height = $tabs.querySelector('li.active').clientHeight + 'px';
-			}
-
-			// Animiate class
-			if ($self.options.animate.toString() == 'true') {
-				web.classAdd(web.element.html, 'tp-animate');
-			}
-		}
+		animate: (typeof $userOptions.animate != 'undefined') ? $userOptions.animate : $tabplateDefault.animate
 	}
 
-	function triggerSetup() {
-		var $triggerLinks = $self.querySelectorAll('li a');
-		var $triggerLength = $triggerLinks.length;
-		web.classAdd($self, 'tabplate-triggers');
-		web.classAdd($self, 'tabplate-count-' + $triggerLength);
-		web.classAdd($self.querySelector('li:first-child'), 'active');
-		for (var $i = $triggerLength - 1; $i >= 0; $i--) {
-			$triggerLinks[$i].onclick = (function() {
-				var $selfI = $i;
-				return function($ev) {
-					$ev.preventDefault();
-					$self.changeTab($selfI);
+	// Variables
+	var $tabContainer = document.querySelector($self.options.tabs);
+	var $triggersContainer = document.querySelector($self.options.selector);
+
+	// Functions
+	var tabTriggers = function() {
+		var $triggers = $triggersContainer.querySelectorAll('a');
+		for (var $i = 0, $len = $triggers.length; $i < $len; $i++) {
+		   $triggers[$i].onclick = function(event) {
+				event.preventDefault();
+				if (this.getAttribute('href') !== null) {
+					web.classRemove($triggersContainer.querySelector('._active'), '_active');
+					web.classAdd(this.parentNode, '_active');
+					tabShow(this.getAttribute('href').substring(1));
 				}
-			})();
+			};
 		}
-	}
+	};
+	var tabsSetup = function() {
+		// Triggers
+		if (web.exists($triggersContainer)) {
+			web.classAdd($triggersContainer, 'tp-triggers');
+			web.classAdd($triggersContainer.querySelector('li:first-child'), '_active');
+			tabTriggers();
+		}
+		// Tabs
+		if (web.exists($tabContainer)) {
+			web.classAdd($tabContainer, 'tp-tabs');
+			web.classAdd($tabContainer.querySelector('li:first-child'), '_active');
+		}
+		// Animate
+		if ($self.options.animate === true) {
+			web.classAdd(web.element.html, 'tp-animate');
+		}
+	};
+	var tabShow = function($id) {
+		web.classRemove($tabContainer.querySelector('._active'), '_active');
+		web.classAdd(document.getElementById($id), '_active');
+	};
 
-	// Calls
-	tabSetup();
-	triggerSetup();
+	// Execute
+	tabsSetup();
 };
