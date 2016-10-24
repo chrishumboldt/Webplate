9262109fc83b16d1039dc0349d0c25578eeab175
Chris Humboldt <chrismodem@gmail.com>
2016-10-24 22:19:36 +0200

    More Webplate stuff.

--- engine/component/buttonplate/js/buttonplate.js
+++ engine/component/buttonplate/js/buttonplate.js
@@ -4,5 +4,91 @@
  * Author: Chris Humboldt
 **/
 
-// Component
-var Buttonplate = function () {};
+// Component container
+var Buttonplate = (function () {
+	// Variables
+	var defaults = {
+		selector: '.button'
+	};
+	var buttonDropClassName = 'buttonplate-drop-down';
+	var documentOnClick = false;
+
+	// Inner component
+	var component = function (button) {
+		// Variables
+		var buttonUL = button.querySelector('ul');
+		// Check
+		if (!buttonUL) {
+			return false;
+		}
+		// Functions
+		var applyButtonDrop = function () {
+			Web.class.add(button, buttonDropClassName);
+			button.onclick = function () {
+				buttonOpen();
+			};
+		};
+		var buttonClose = function () {
+			Web.class.remove(buttonUL, '_open');
+		};
+		var buttonOpen = function () {
+			closeAll();
+			buttonUL.style.width = button.clientWidth + 'px';
+			setTimeout(function () {
+				Web.class.add(buttonUL, '_open');
+			});
+		};
+		// Execute and return
+		applyButtonDrop();
+		return {
+			button: button,
+			close: buttonClose,
+			open: buttonOpen
+		};
+	};
+
+	// Functions
+	var closeAll = function () {
+		var openDropDowns = document.querySelectorAll('.' + buttonDropClassName + ' ul._open');
+		for (var i = 0, len = openDropDowns.length; i < len; i++) {
+			Web.class.remove(openDropDowns[i], '_open');
+		}
+	};
+	var setup = function () {
+		if (!Web.is.touch()) {
+			Web.class.add(Web.element.html, 'bp-no-touch');
+		}
+		if (!documentOnClick) {
+			documentOnClick = true;
+			Web.event.add(document, 'click', function () {
+				closeAll();
+			});
+		}
+	};
+
+	// Initialiser
+	var init = function (userOptions) {
+		var userOptions = (typeof userOptions === 'object') ? userOptions : false;
+		var options = {
+			selector: userOptions.selector || defaults.selector
+		};
+		var buttons = document.querySelectorAll(options.selector);
+		if (buttons.length < 1) {
+			return false;
+		}
+		// Initialise each and return
+		var objReturn = [];
+		for (var i = 0, len = buttons.length; i < len; i++) {
+		   objReturn.push(new component(buttons[i]));
+		}
+		return objReturn;
+	};
+
+	// Execute and return
+	setup();
+	return {
+		defaults: defaults,
+		closeAll: closeAll,
+		init: init
+	}
+})();
