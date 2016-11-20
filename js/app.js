7489e3cd53999b33adf98ccf559eb87b8ecc8654
Chris Humboldt <chrismodem@gmail.com>
2016-11-20 23:20:56 +0200

    Massive changes

---
+++ js/app.js
@@ -0,0 +1,7 @@
+/**
+ * File: js/app.js
+ * Type: Javascript app file
+ * Author: Chris Humboldt
+**/
+
+Rocket.log('The app file loads successfully...');
--- js/tester.min.js
+++
@@ -1,1 +0,0 @@
-console.log("The test script works!");
