4d69ceada8adea0aad41668447a9e1f730cab575
Chris Humboldt <chrismodem@gmail.com>
2017-02-20 07:34:19 +0200

    Updates to core and modules.

--- engine/build/js/core.js
+++ engine/build/js/core.js
@@ -9,20 +9,16 @@ Rocket require
 This module is baked directly into Rocket as it is a requirement.
 The same is true for the Rocket Tools.
 */
-var Rocket = (typeof Rocket === 'object') ? Rocket : {};
-if (!Rocket.defaults) {
-    Rocket.defaults = {};
-}
 Rocket.defaults.require = {
     errors: true,
-    rootUrl: './'
+    rootPath: './node_modules/'
 };
 var RockMod_Module;
 (function (RockMod_Module) {
     var listModules = {};
     var moduleMethods = {
         add: function (obj) {
-            if (typeof obj !== 'object') {
+            if (!Rocket.is.object(obj)) {
                 return false;
             }
             for (var key in obj) {
@@ -31,13 +27,13 @@ var RockMod_Module;
                         listModules[key] = {
                             loaded: false
                         };
-                        if (typeof obj[key].css === 'string' || isArrayCheck(obj[key].css)) {
-                            listModules[key].css = obj[key].css;
+                        if (Rocket.is.string(obj[key].css) || Rocket.is.array(obj[key].css)) {
+                            listModules[key].css = moduleMethods.sanitisePaths(obj[key].css);
                         }
-                        if (typeof obj[key].js === 'string' || isArrayCheck(obj[key].js)) {
-                            listModules[key].js = obj[key].js;
+                        if (Rocket.is.string(obj[key].js) || Rocket.is.array(obj[key].js)) {
+                            listModules[key].js = moduleMethods.sanitisePaths(obj[key].js);
                         }
-                        if (RockMod_Module.isArray(obj[key].requires)) {
+                        if (Rocket.is.array(obj[key].requires)) {
                             listModules[key].requires = obj[key].requires;
                         }
                     }
@@ -45,7 +41,7 @@ var RockMod_Module;
             }
         },
         dependencies: function (name) {
-            if (typeof name !== 'string') {
+            if (!Rocket.is.string(name)) {
                 return false;
             }
             var dependencies = [];
@@ -53,7 +49,7 @@ var RockMod_Module;
                 if (moduleMethods.exists(name)) {
                     dependencies.push(name);
                     var thisModule = listModules[name];
-                    if (isArrayCheck(thisModule.requires)) {
+                    if (Rocket.is.array(thisModule.requires)) {
                         for (var _i = 0, _a = thisModule.requires; _i < _a.length; _i++) {
                             var moduleName = _a[_i];
                             if (dependencies.indexOf(moduleName) > -1) {
@@ -74,23 +70,23 @@ var RockMod_Module;
             return dependencies;
         },
         exists: function (name) {
-            if (typeof name !== 'string') {
+            if (!Rocket.is.string(name)) {
                 return false;
             }
-            return (typeof listModules[name] === 'undefined') ? false : true;
+            return Rocket.exists(listModules[name]);
         },
         get: function (name) {
-            if (typeof name !== 'string') {
+            if (!Rocket.is.string(name)) {
                 return false;
             }
             return listModules[name];
         },
         isLoaded: function (name) {
-            if (typeof name !== 'string') {
+            if (!Rocket.is.string(name)) {
                 return false;
             }
             var thisModule = listModules[name];
-            return (typeof thisModule === 'object') ? thisModule.loaded : false;
+            return (Rocket.is.object(thisModule)) ? thisModule.loaded : false;
         },
         listLoaded: function () {
             var listLoaded = [];
@@ -102,10 +98,19 @@ var RockMod_Module;
             return listLoaded;
         },
         remove: function (name) {
-            if (typeof name !== 'string' || listModules[name] === 'undefined') {
+            if (!Rocket.is.string(name) || !Rocket.exists(listModules[name])) {
                 return false;
             }
             delete listModules[name];
+        },
+        sanitisePaths: function (paths) {
+            if (Rocket.is.string(paths)) {
+                paths = [paths];
+            }
+            for (var len = paths.length, i = 0; i < len; i++) {
+                paths[i] = paths[i].replace(/~\//g, Rocket.defaults.require.rootPath);
+            }
+            return paths;
         }
     };
     var validate = {
@@ -113,22 +118,22 @@ var RockMod_Module;
             var hasCSS = false;
             var hasJS = false;
             var hasRequires = false;
-            if (typeof name !== 'string') {
+            if (!Rocket.is.string(name)) {
                 return false;
             }
-            if (typeof obj !== 'object') {
+            if (!Rocket.is.object(obj)) {
                 return false;
             }
-            if (typeof obj.css === 'undefined' && typeof obj.js === 'undefined') {
+            if (!Rocket.exists(obj.css) && !Rocket.exists(obj.js)) {
                 return false;
             }
-            if ((typeof obj.css === 'string' || isArrayCheck(obj.css)) && obj.css.length > 0) {
+            if ((Rocket.is.string(obj.css) || Rocket.is.array(obj.css) && obj.css.length > 0)) {
                 hasCSS = true;
             }
-            if ((typeof obj.js === 'string' || isArrayCheck(obj.js)) && obj.js.length > 0) {
+            if ((Rocket.is.string(obj.js) || Rocket.is.array(obj.js) && obj.js.length > 0)) {
                 hasJS = true;
             }
-            if (isArrayCheck(obj.requires) && obj.requires.length > 0) {
+            if (Rocket.is.array(obj.requires) && obj.requires.length > 0) {
                 hasRequires = true;
             }
             if (!hasCSS && !hasJS && !hasRequires) {
@@ -137,13 +142,9 @@ var RockMod_Module;
             return true;
         }
     };
-    function isArrayCheck(check) {
-        return (typeof check === 'object' && check instanceof Array) ? true : false;
-    }
     RockMod_Module.add = moduleMethods.add;
     RockMod_Module.exists = moduleMethods.exists;
     RockMod_Module.get = moduleMethods.get;
-    RockMod_Module.isArray = isArrayCheck;
     RockMod_Module.isLoaded = moduleMethods.isLoaded;
     RockMod_Module.dependencies = moduleMethods.dependencies;
     RockMod_Module.list = listModules;
@@ -152,150 +153,156 @@ var RockMod_Module;
 })(RockMod_Module || (RockMod_Module = {}));
 var RockMod_Require;
 (function (RockMod_Require) {
-   function loadFile(file, thisCallback, customRootUrl) {
-      var callback = (typeof thisCallback === 'function') ? thisCallback : function () { };
-      var theInclude;
-      var type;
-      var rootUrl = (typeof customRootUrl === 'string') ? customRootUrl : Rocket.defaults.require.rootUrl;
-      var filePath = (isURL(file)) ? file : rootUrl + file;
-      if (/(.css)$/.test(file)) {
-           type = 'css';
-           theInclude = document.createElement('link');
-           theInclude.rel = 'stylesheet';
-           theInclude.href = filePath;
-      }
-      else if (/(.js)$/.test(file)) {
-           type = 'js';
-           theInclude = document.createElement('script');
-           theInclude.setAttribute('async', true);
-           theInclude.src = filePath;
-      }
-      theInclude.onload = function () {
-           if (type !== 'js' && Object.hasOwnProperty.call(window, "ActiveXObject") && !window['ActiveXObject']) {
-               return callback(false);
-           }
-           return callback(true);
-      };
-      theInclude.onreadystatechange = function () {
-           if (!this.readyState || this.readyState === 'loaded' || this.readyState === 'complete') {
-               this.onreadystatechange = null;
-               if (type === 'js') {
-                   return callback(false);
-               }
-               return callback(true);
-           }
-      };
-      theInclude.onerror = function () {
-           return callback(false);
-      };
-      document.getElementsByTagName('head')[0].appendChild(theInclude);
-   }
-   function loadModuleFiles(name, thisModule, callback) {
-      var count = 0;
-      var files = [];
-      if (typeof thisModule.css === 'string') {
-           files.push(thisModule.css);
-      }
-      else if (Rocket.module.isArray(thisModule.css) && thisModule.css.length > 0) {
-           files = files.concat(thisModule.css);
-      }
-      if (typeof thisModule.js === 'string') {
-           files.push(thisModule.js);
-      }
-      else if (Rocket.module.isArray(thisModule.js) && thisModule.js.length > 0) {
-           files = files.concat(thisModule.js);
-      }
-      count = files.length;
-      if (count < 1) {
-           return callback(false);
-      }
-      for (var _i = 0, files_1 = files; _i < files_1.length; _i++) {
-           var file = files_1[_i];
-           loadFile(file, function (response) {
-               count--;
-               if (count === 0) {
-                   return callback(true);
-               }
-           }, false);
-      }
-   }
-   function isURL(check) {
-      return /^(https?:\/\/[^\s]+)/.test(check);
-   }
-   var Require = (function () {
-      function Require() {
-           this.modules = [];
-           this.modulesCount = 0;
-      }
-      Require.prototype.add = function (name) {
-           if (typeof name !== 'string' || !Rocket.module.exists(name) || this.modules.indexOf(name) > -1) {
-               return false;
-           }
-           this.modules = this.modules.concat(Rocket.module.dependencies(name));
-           this.modules = this.modules.filter(function (value, index, self) {
-               return self.indexOf(value) === index;
-           });
-           this.modulesCount = this.modules.length;
-      };
-      Require.prototype.load = function (callback) {
-           var listModules = this.modules;
-           var modulesCount = this.modulesCount;
-           function loadModules(names, callback) {
-               var namesCount = names.length;
-               function loadModulesDone() {
-                   modulesCount--;
-                   namesCount--;
-                   if (namesCount === 0) {
-                       callback();
-                   }
-               }
-               var _loop_1 = function (name_1) {
-                   if (!Rocket.module.exists(name_1)) {
-                       if (Rocket.defaults.require.errors) {
-                           throw new Error('ROCKET REQUIRE: You are missing a required module: ' + name_1);
-                       }
-                   }
-                   else {
-                       if (Rocket.module.isLoaded(name_1)) {
-                           loadModulesDone();
-                       }
-                       else {
-                           var thisModule_1 = Rocket.module.get(name_1);
-                           var dependencies = (Rocket.module.isArray(thisModule_1.requires) && thisModule_1.requires.length > 0) ? thisModule_1.requires : false;
-                           thisModule_1.loaded = true;
-                           if (!dependencies) {
-                              loadModuleFiles(name_1, thisModule_1, function () {
-                                   loadModulesDone();
-                              });
-                           }
-                           else {
-                              loadModules(dependencies, function () {
-                                   loadModuleFiles(name_1, thisModule_1, function () {
-                                       loadModulesDone();
-                                   });
-                              });
-                           }
-                       }
-                   }
-               };
-               for (var _i = 0, names_1 = names; _i < names_1.length; _i++) {
-                   var name_1 = names_1[_i];
-                   _loop_1(name_1);
-               }
-           }
-           for (var _i = 0, _a = this.modules; _i < _a.length; _i++) {
-               var thisModule = _a[_i];
-               loadModules([thisModule], function () {
-                   if (modulesCount === 0 && typeof callback === 'function') {
-                       return callback();
-                   }
-               });
-           }
-      };
-      return Require;
-   }());
-   RockMod_Require.newRequire = Require;
-   RockMod_Require.load = loadFile;
+    function loadFile(file, callback, customRootPath) {
+        var theInclude;
+        var type;
+        var rootUrl = (Rocket.is.string(customRootPath)) ? customRootPath : '';
+        var filePath = (Rocket.is.url(file)) ? file : rootUrl + file;
+        if (/(.css)$/.test(file)) {
+            type = 'css';
+            theInclude = document.createElement('link');
+            theInclude.rel = 'stylesheet';
+            theInclude.href = filePath;
+        }
+        else if (/(.js)$/.test(file)) {
+            type = 'js';
+            theInclude = document.createElement('script');
+            theInclude.setAttribute('async', true);
+            theInclude.src = filePath;
+        }
+        theInclude.onload = function () {
+            if (type !== 'js' && Object.hasOwnProperty.call(window, "ActiveXObject") && !window['ActiveXObject']) {
+                if (Rocket.is.function(callback)) {
+                    return callback(false);
+                }
+            }
+            if (Rocket.is.function(callback)) {
+                return callback(true);
+            }
+        };
+        theInclude.onreadystatechange = function () {
+            if (!this.readyState || this.readyState === 'loaded' || this.readyState === 'complete') {
+                this.onreadystatechange = null;
+                if (type === 'js' && Rocket.is.function(callback)) {
+                    return callback(false);
+                }
+                if (Rocket.is.function(callback)) {
+                    return callback(true);
+                }
+            }
+        };
+        theInclude.onerror = function () {
+            if (Rocket.is.function(callback)) {
+                return callback(false);
+            }
+        };
+        document.getElementsByTagName('head')[0].appendChild(theInclude);
+    }
+    function loadModuleFiles(name, thisModule, callback) {
+        var count = 0;
+        var files = [];
+        if (Rocket.is.string(thisModule.css)) {
+            files.push(thisModule.css);
+        }
+        else if (Rocket.is.array(thisModule.css) && thisModule.css.length > 0) {
+            files = files.concat(thisModule.css);
+        }
+        if (Rocket.is.string(thisModule.js)) {
+            files.push(thisModule.js);
+        }
+        else if (Rocket.is.array(thisModule.js) && thisModule.js.length > 0) {
+            files = files.concat(thisModule.js);
+        }
+        count = files.length;
+        if (count < 1) {
+            return callback(false);
+        }
+        for (var _i = 0, files_1 = files; _i < files_1.length; _i++) {
+            var file = files_1[_i];
+            loadFile(file, function (response) {
+                count--;
+                if (count === 0) {
+                    return callback(true);
+                }
+            }, false);
+        }
+    }
+    var Require = (function () {
+        function Require() {
+            this.modules = [];
+        }
+        Require.prototype.add = function (name) {
+            if (!Rocket.is.string(name)
+                || !Rocket.module.exists(name)
+                || Rocket.module.isLoaded(name)
+                || this.modules.indexOf(name) > -1) {
+                return false;
+            }
+            this.modules = this.modules.concat(Rocket.module.dependencies(name));
+            this.modules = Rocket.array.unique(this.modules);
+        };
+        Require.prototype.load = function (callback) {
+            var self = this;
+            var listModules = self.modules.reverse();
+            var modulesCount = self.modulesCount;
+            function loadExecute() {
+                loadModules(listModules, function () {
+                    self.modules = [];
+                    if (Rocket.is.function(callback)) {
+                        return callback();
+                    }
+                });
+            }
+            function loadModules(modules, callback) {
+                var count = modules.length;
+                for (var _i = 0, modules_1 = modules; _i < modules_1.length; _i++) {
+                    var thisModule = modules_1[_i];
+                    loadModule(thisModule, function () {
+                        count--;
+                        if (count === 0) {
+                            return callback();
+                        }
+                    });
+                }
+            }
+            function loadModule(name, callback) {
+                if (!Rocket.module.exists(name)) {
+                    if (Rocket.defaults.require.errors) {
+                        throw new Error('ROCKET REQUIRE: You are missing a required module: ' + name);
+                    }
+                }
+                else {
+                    if (Rocket.module.isLoaded(name)) {
+                        return callback();
+                    }
+                    else {
+                        var thisModule_1 = Rocket.module.get(name);
+                        var dependencies = (Rocket.is.array(thisModule_1.requires) && thisModule_1.requires.length > 0) ? thisModule_1.requires : false;
+                        thisModule_1.loaded = true;
+                        if (!dependencies) {
+                            loadModuleFiles(name, thisModule_1, function () {
+                                return callback();
+                            });
+                        }
+                        else {
+                            loadModules(dependencies, function () {
+                                loadModuleFiles(name, thisModule_1, function () {
+                                    return callback();
+                                });
+                            });
+                        }
+                    }
+                }
+            }
+            function removeModule(name) {
+                listModules.splice(listModules.indexOf(name), 1);
+            }
+            loadExecute();
+        };
+        return Require;
+    }());
+    RockMod_Require.newRequire = Require;
+    RockMod_Require.load = loadFile;
 })(RockMod_Require || (RockMod_Require = {}));
 Rocket.module = RockMod_Module;
 Rocket.require = function () {
@@ -328,6 +335,7 @@ by the cockpit.json file.
       module: pathRoot + 'node_modules/',
       root: pathRoot
    };
+
    Rocket.module.add({
 		fastclick: {
 			js: path.engine.module + 'fastclick/lib/fastclick.js'
@@ -353,16 +361,19 @@ by the cockpit.json file.
 		load: {
 			modules: function (callback) {
 				var rootModules = [];
+
 				// Prefined modules
 				if (Rocket.is.object(cockpit.engine) && Rocket.is.boolean(cockpit.engine.light) && cockpit.engine.light === true) {
 					rootModules.push('rocketLight');
 				} else {
 					rootModules.push('rocket');
 				}
+
             // Touch inclusion
             if (Modernizr.touchevents) {
                rootModules.push('fastclick');
             }
+
             // Load other modules
             var app = {
                css: Rocket.helper.setDefault(cockpit.css, []),
@@ -503,6 +514,7 @@ by the cockpit.json file.
 				loaderDiv.style.fontFamily = 'Arial, Helvetica, sans-serif';
 				loaderDiv.style.textAlign = 'center';
 				document.getElementsByTagName('body')[0].appendChild(loaderDiv);
+            
 				var pageLoaderTimer = setInterval(function () {
 					i++;
 					if (document.getElementById('rocket-page-loader') !== null) {
