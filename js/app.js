02be60d0d35e981b02722e69a2e24f9cd95d3b27
Chris Humboldt <chrismodem@gmail.com>
2017-03-07 13:53:01 +0200

    Updates to Modal and Message modules.

--- js/app.js
+++ js/app.js
@@ -3,3 +3,15 @@ Author: Chris Humboldt
 */
 
 Rocket.log('The app file loads successfully...');
+
+Rocket.modal({
+   heading: 'This is a test',
+   buttons: true,
+   body: 'Body here.',
+   onTrue: () => {
+      Rocket.message({
+         body: 'Woot'
+      });
+      Rocket.log('ok');
+   }
+});
